056c74d79dd227bf987d432c2682fe516853f928
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-10 12:26:35 -0700

    seedsWorking

--- seeds/01_users_seeds.js
+++ seeds/01_users_seeds.js
@@ -4,11 +4,11 @@
  */
 exports.seed = async function(knex) {
     await knex('users').insert([
-    {username:'Marcy-Ann',password:'1234',email:'',first_name: 'Ann', last_name: 'Duong',phonenumber:'',bio:''},
-    {username:'Marcy-maya',password:'5678',email:'',first_name: 'Maya', last_name: 'Bhattacharjee',phonenumber:'',bio:''},
-    // {username:'Marcy-reuben',password:'9876',email:'',first_name: 'Reuben', last_name: 'Ogbonna',phonenumber:'',bio:''},
-    // {username:'Marcy-Erwin',password:'1212',email:'',first_name: 'Erwin', last_name: 'Saget',phonenumber:'',bio:''},
-    // {username:'Marcy-Motun',password:'2121',email:'',first_name: 'Motun', last_name: 'Bolumole',phonenumber:'',bio:''},
-    // {username:'Marcy-Laura',password:'1313',email:'',first_name: 'Laura', last_name: 'Rouse',phonenumber:'',bio:''}
+    {username:'Marcy-Ann',password:'1234',email:'',firstname: 'Ann', lastname: 'Duong',phonenumber:'',bio:''},
+    {username:'Marcy-maya',password:'5678',email:'',firstname: 'Maya', lastname: 'Bhattacharjee',phonenumber:'',bio:''},
+    // {username:'Marcy-reuben',password:'9876',email:'',firstname: 'Reuben', lastname: 'Ogbonna',phonenumber:'',bio:''},
+    // {username:'Marcy-Erwin',password:'1212',email:'',firstname: 'Erwin', lastname: 'Saget',phonenumber:'',bio:''},
+    // {username:'Marcy-Motun',password:'2121',email:'',firstname: 'Motun', lastname: 'Bolumole',phonenumber:'',bio:''},
+    // {username:'Marcy-Laura',password:'1313',email:'',firstname: 'Laura', lastname: 'Rouse',phonenumber:'',bio:''}
   ]);
 };
