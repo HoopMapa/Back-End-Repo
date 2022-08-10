bb7287f748cdf21e8908af882e84982bd87e2edd
shevaughn98 <shevaughnlegall@gmail.com>
2022-08-10 19:44:44 -0400

    courtTable

--- migrations/20220810213231_court.js
+++ migrations/20220810213231_court.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('courts', (table) => {
+        table.increments('id').primary();
+        table.string('court_name')
+        table.string('court_level')
+        table.string('court_location')
+        table.integer('user_id').references('id').inTable('users');
+        table.string('basketball_court')
+      })
 };
 
 /**
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('courts')
 };
--- seeds/001_delete_entries.js
+++ seeds/001_delete_entries.js
@@ -3,6 +3,7 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
+  await knex('courts').del()
   await knex('posts').del()
   await knex('users').del()
 };
