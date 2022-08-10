2c1abb9ce3888887c413ede213e2e9252a48070a
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-10 12:05:55 -0700

    seed/migrations

--- migrations/20220804200220_users.js
+++ migrations/20220804200220_users.js
@@ -2,24 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable("users", (table) => { 
-    table.increments.length('id').primary()
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments('id').primary()
     table.string('username').notNullable()
     table.string('password').notNullable()
     table.string('email').notNullable()
-    table.string('firstname').notNullable()
-    table.string('lastname').notNullable()
-    table.string('phonenumber').notNullable()
-    table.string('bio').nullable();
-  }
-  )
+    table.string('firstname')
+    table.string('lastname')
+    table.string('phonenumber')
+    table.string('bio')
+  })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.down = function(knex) {
-    return knex.schema.dropTable("users")
-  };
+exports.down = function (knex) {
+  return knex.schema.dropTable("users")
+};
