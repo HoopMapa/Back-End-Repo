72178eb3c546a7891514b2a90c9f9199f315d8f4
shevaughn98 <shevaughnlegall@gmail.com>
2022-08-24 13:21:03 -0400

    update users

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,12 @@ class Users {
         return dbResult.rows;
     }
 
+    static async updateUserFromDB({ username, phonenumber, bio, user_id}) {
+        const sql = `UPDATE users SET username = ($1) , phonenumber = ($2), bio = ($3) WHERE users.id = ($4) RETURNING *` 
+        const dbResult = await pool.query(sql, [ username, phonenumber, bio, user_id])
+        return dbResult.rows;
+    }
+    
     static async deleteUserFromDB(user_id) {
         await pool.query(`DELETE FROM posts WHERE user_id = ($1) RETURNING *`, [user_id]);
         await pool.query(`DELETE FROM users WHERE id = ($1) RETURNING *`, [user_id]);
