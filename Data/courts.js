610f3653f0892bf0bc34aa7e1c2437408f539107
shevaughn98 <shevaughnlegall@gmail.com>
2022-08-30 15:25:53 -0400

    All routes Working/added courts Data dir./ seeded courts table

---
+++ Data/courts.js
@@ -0,0 +1,198 @@
+const CourtsData = [{
+    "Prop_ID": "X159",
+    "Name": "Captain William Harry Thompson Playground",
+    "Location": "E 174 St. & Bronx River Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8342",
+    "lon": "-73.8775"
+},
+{
+    "Prop_ID": "X179",
+    "Name": "52 Playground",
+    "Location": "Kelly St. & Ave. St. John",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8149",
+    "lon": "-73.9021"
+},
+{
+    "Prop_ID": "X216",
+    "Name": "Abigail Adams Playground",
+    "Location": "Tinton Ave. & E 156 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8172",
+    "lon": "-73.9044"
+},
+{
+    "Prop_ID": "X169",
+    "Name": "Agnes Haywood Playground",
+    "Location": "Barnes Ave. & E 215 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8804",
+    "lon": "-73.8619"
+},
+{
+    "Prop_ID": "X172",
+    "Name": "Allerton Playground",
+    "Location": "Bouck & Throop Aves.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8659",
+    "lon": "-73.8503"
+},
+{
+    "Prop_ID": "X187",
+    "Name": "Angelo Campanero Playground",
+    "Location": "Gun Hill Rd., Eastchester Rd. and O'Neill Pl.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8679",
+    "lon": "-73.843"
+},
+{
+    "Prop_ID": "X001",
+    "Name": "Aqueduct Lands Playground",
+    "Location": "W 183 St., Aqueduct Ave., W Tremont Ave. and University Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8634",
+    "lon": "-73.9035"
+},
+{
+    "Prop_ID": "X219",
+    "Name": "Arcilla Playground",
+    "Location": "E 164 St. & Teller Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8271",
+    "lon": "-73.9145"
+},
+{
+    "Prop_ID": "X150J",
+    "Name": "Bailey Playground",
+    "Location": "Bailey Ave. & W 234-W 238 Sts.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8811",
+    "lon": "-73.9008"
+},
+{
+    "Prop_ID": "X166",
+    "Name": "Behagen Playground",
+    "Location": "E 166 St., Union Ave. and Tinton Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8254",
+    "lon": "-73.9015"
+},
+{
+    "Prop_ID": "X108",
+    "Name": "Belmont Playground",
+    "Location": "Crotona Ave. & E 181 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8497",
+    "lon": "-73.8875"
+},
+{
+    "Prop_ID": "X002",
+    "Name": "Brady Playground",
+    "Location": "Bronxdale Ave. & Bronx Pk E",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8512",
+    "lon": "-73.8705"
+},
+{
+    "Prop_ID": "X148A3",
+    "Name": "Bridge Playground",
+    "Location": "Boscobel Pl., University Ave. and W 171 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8443",
+    "lon": "-73.924"
+},
+{
+    "Prop_ID": "X183",
+    "Name": "Bruckner Playground",
+    "Location": "Phillips Ave. & E 177 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8268",
+    "lon": "-73.8289"
+},
+{
+    "Prop_ID": "X121",
+    "Name": "Bufano Playground",
+    "Location": "Bradford, Edison, La Salle and Waterbury Aves.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.838",
+    "lon": "-73.8312"
+},
+{
+    "Prop_ID": "X194",
+    "Name": "Capt. Rivera Playground",
+    "Location": "Forest Ave. & E 156 St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8179",
+    "lon": "-73.907"
+},
+{
+    "Prop_ID": "X160",
+    "Name": "Caserta Playground",
+    "Location": "St. Raymond Ave. & Odell St.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8372",
+    "lon": "-73.854"
+},
+{
+    "Prop_ID": "X111",
+    "Name": "Cedar Playground",
+    "Location": "W 179 St. & Sedgwick Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8548",
+    "lon": "-73.9177"
+},
+{
+    "Prop_ID": "X008",
+    "Name": "Claremont Park",
+    "Location": "E 170 St. & Teller Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8378",
+    "lon": "-73.9082"
+},
+{
+    "Prop_ID": "X200",
+    "Name": "Clark Playground",
+    "Location": "E 144 St. & 3 Ave.",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8136",
+    "lon": "-73.9211"
+},
+{
+    "Prop_ID": "X010",
+    "Name": "Crotona Playground #11",
+    "Location": "Fulton Ave. between E 172 St. and Claremont",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8384",
+    "lon": "-73.8993"
+},
+{
+    "Prop_ID": "X010",
+    "Name": "Crotona Playground #2",
+    "Location": "Crotona N between Marmion and Prospect",
+    "Num_of_Courts": null,
+    "Accessible": "N",
+    "lat": "40.8402",
+    "lon": "-73.8913"
+}]
